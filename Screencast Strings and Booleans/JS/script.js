fa2da7aa5062807de9ba3aee19723270efdfcfcc
Arlene Smith <smitharlene918@gmail.com>
2013-11-03 21:56:40 -0500

    following along the screencast for strings, numbers and booleans

---
+++ Screencast Strings and Booleans/JS/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Arlene on 11/3/13.
+ */
+// Variables of different types, Numbers, Strings and Booleans
+
+var age = 38;
+
+var isStudent = false; //Boolean Variable
+
+var firstName = "Arlene";
+
+var phrase = 'I don\'t know \n You\'ll have to ask the professor'
