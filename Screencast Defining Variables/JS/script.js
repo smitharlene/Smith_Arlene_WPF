c3f05face12301d934aaad59e4ff2785a0fd5504
Arlene Smith <smitharlene918@gmail.com>
2013-11-03 21:35:08 -0500

    Initial following along the screencast for defining variables

---
+++ Screencast Defining Variables/JS/script.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Arlene on 11/3/13.
+ */
+// Defining Variables
+
+var age;// declaration
+
+age= 38;//defining
+
+// storage container = value
+
+var yearOfBirth =1970; //declaring and defining on the same line
