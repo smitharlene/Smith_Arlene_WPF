40434ff17272a286bba41b9e279bc1c353c8fce2
Arlene Smith <smitharlene918@gmail.com>
2013-11-03 22:25:46 -0500

    following along screencast on arrays

---
+++ Screencast Arrays/JS/script.js
@@ -0,0 +1,7 @@
+//Arrays
+
+
+var muppetNames = ["kermit", "Miss Piggy", "Gonzo", "Rizzo"];// 0,1 ,2,3
+
+muppetNames[1] = "Rizzo";// change index #1 to Rizzo
+console.log(muppetNames[1]);// lets me see whats in line #1
