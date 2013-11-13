35f73f4f7bffd07fb3c43c1929cd999a3abe6812
Arlene Smith <smitharlene918@gmail.com>
2013-11-12 21:51:32 -0500

    Following along the screencast for conditionals

---
+++ Screencast Conditionals/JS/script.js
@@ -0,0 +1,16 @@
+/**
+ * Created by Arlene on 11/11/13.
+ */
+
+var oldEnough = false;
+
+//if the child is old enough, print to the console "you can ride!"
+
+if (oldEnough){
+
+    //code performed if condition is true
+
+    console.log("You can ride the coaster");
+}
+
+console.log("what comes after");
