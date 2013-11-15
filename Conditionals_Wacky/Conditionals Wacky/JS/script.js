f03ca36c6de06ccdb5187d6192d965dc67f77d95
Arlene Smith <smitharlene918@gmail.com>
2013-11-14 23:58:48 -0500

    added comments

--- Conditionals_Wacky/Conditionals Wacky/JS/script.js
+++ Conditionals_Wacky/Conditionals Wacky/JS/script.js
@@ -9,13 +9,15 @@
 // I want to take my family to Disney world.  I can only afford to go if my christmas bonus and paycheck are $900 or more or
     // the cost of the disney tickets is $400 or less.
 
-var christmasBonus = 200;
+var christmasBonus = 200;  //givens
 var paycheck = 500;
 var disneyTickets = 500;
 
-
+// if christmas bonus + paycheck are greater than or equal to $900  or  if the disney tickets are $400 or less, I can take my family to disney.
 if(christmasBonus + paycheck >= 900 || disneyTickets <= 400){
     console.log("I can take my family to disney!")
+
+    //if they are not, then I will need to save more money
 }else{
     console.log("I need to save more money to be able to go to disney.")
 }
