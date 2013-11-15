f71e6e6bdeb739e4888a1f62126d9c831eb57f6d
Arlene Smith <smitharlene918@gmail.com>
2013-11-14 22:17:50 -0500

    Initial commit for Conditionals Personal Assignment

---
+++ Conditionals_Personal/Conditionals Personal/JS/script.js
@@ -0,0 +1,26 @@
+
+/**
+ * Created by Arlene on 11/14/13.
+ */
+
+//Arlene Smith
+//November 14, 2013
+//Conditionals Personal Assignment
+
+
+//We are picking up family members from the airport.  I can fit 4 passengers in my SUV,
+// if the amount of passengers is more than that I will need to rent a car.
+//Can I pick them up in my car or do I have to rent a bigger car?
+
+var familyMembers = 5 ;
+var suvPassengersCapacity = 4;
+
+//family members will have to equal or be less than my passenger capacity.  If it's less, I can pick them up in my car.  If it's more I need to rent a car.
+
+(familyMembers <= suvPassengersCapacity) ? console.log("You can pick up the family in the SUV.") : console.log("You will need to rent a bigger car.");
+
+
+
+
+
+
