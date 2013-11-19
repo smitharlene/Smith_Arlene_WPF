222baf67ea4c141a52089b830b06f03f00535f5c
Arlene Smith <smitharlene918@gmail.com>
2013-11-18 22:17:07 -0500

    Initial commit for screencast functions (Basic)

---
+++ Screencast Functions/JS/script.js
@@ -0,0 +1,22 @@
+/**
+ * Created by Arlene on 11/18/13.
+ */
+
+
+//Screencast Functions
+
+//Basic Functions
+
+
+function outptMsg(){
+    console.log("Hello World");
+
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+
+}
