d03ab536084319cd7507d28d99ee9e85b78cd7d1
Arlene Smith <smitharlene918@gmail.com>
2013-11-19 22:27:47 -0500

    Initial commit to Functions Worksheet Assignment

---
+++ Functions_worksheet/Functions Worksheet/JS/script.js
@@ -0,0 +1,26 @@
+/**
+ * Created by Arlene on 11/19/13.
+ */
+
+
+// Arlene Smith
+// November 19, 2013
+
+
+
+// Circumference
+// Calculate the circumference of a circle
+
+
+
+
+var pie= 3.14;
+var total = calcCircle(pie,10)
+
+function calcCircle (pie,radius){
+    var circumferenceCircle = (2 * pie * radius);
+
+    return circumferenceCircle;
+}
+console.log("The circumference of the circle is " + total);
+
