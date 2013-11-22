ef06f30acde63ea0bf9922dbb1b594156c486a92
Arlene Smith <smitharlene918@gmail.com>
2013-11-21 21:14:12 -0500

    added comments to my assignment

--- Functions_Personal/Functions Personal/JS/script.js
+++ Functions_Personal/Functions Personal/JS/script.js
@@ -14,29 +14,31 @@
 
 
 
-var price = 35;   
-var discount = 5;
+var price = 35;   //This variable represents the price of the ticket
+var discount = 5;  //$5 off discount coupon
 
 
-if(price <= 30){
+if(price <= 30){     //If the price of the ticket is less or equal to $30 then I can go to the comedy club
     console.log("With a price of $" + price + ", I can go to the comedy club.");
 
-}else if(price - discount <= 30){
+}else if(price - discount <= 30){   //If the price of the ticket minus the $5 off discount is less than $30, then I can go
     console.log("After the $" + discount + " off discount, I can afford to go to the comedy club.")
 
 
-}else {
+}else {  //If the price of the ticket are more that $30 after using the discount then I would have to save more money
     console.log("The ticket is $" +price + ", I need to save more money.")
 
 
 }
 
-var budgetTotal = calcNightExpenses ( 30,50);
+// This function calculates my total expenses for the night including my $30 comedy club ticket and $50 for dinner
 
-function calcNightExpenses (ticket, dinner){
+var budgetTotal = calcNightExpenses ( 30,50);  //This variable is use to be able to print out the function info into the console which = to the function call
 
-    var expenses = ticket + dinner;
+function calcNightExpenses (ticket, dinner){ //This contains my function name and parameters
 
-    return expenses;
+    var expenses = ticket + dinner; // calculating my expense
+
+    return expenses; //this is what gives the information out
 }
-console.log("My total expenses for the night are $"+ budgetTotal);
+console.log("My total expenses for the night are $"+ budgetTotal); //what's printing out in my console
