5726167704718e039adf35226e2694b66f2b2b0c
Arlene Smith <smitharlene918@gmail.com>
2013-11-21 21:38:08 -0500

    Initial commit for Functions Wacky Project

---
+++ Functions_Wacky/Functions Wacky/JS/script.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Arlene on 11/21/13.
+ */
+
+
+//Arlene Smith
+//November 21, 2013
+//Functions Wacky Assignment
+
+
+
+//If the Texans wins are greater than 7 games or score 30 points a game, they keep their current coach.  If not, they will get a new coach.
+
+var texansWins = 10;
+var gamePoints = 15;
+
+(texansWins > 7 || gamePoints >= 30)? console.log("The Texans can keep their current coach!"): console.log("The Texans will need to get a new coach!");
+
+
+
