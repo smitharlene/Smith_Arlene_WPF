121dc1194e97923f7e2728ee69ae5805f3048576
Arlene Smith <smitharlene918@gmail.com>
2013-11-21 23:36:17 -0500

    correcting grammatical errors

--- Functions_Industry/Functions Industry/JS/script.js
+++ Functions_Industry/Functions Industry/JS/script.js
@@ -11,14 +11,14 @@
 
 //Customer Service- Inbound calls
 
-//I work at Customer service call center and I need to calculate my average calls per month with the information I have.
+//I work at a customer service call center and I need to calculate my average calls per month with the information I have.
 //Which is I take 60 calls a day, I work 5 days of the week and I work 4 weeks a month.
 
 //Calculate the amount of calls I take per month
 
 //Parameters (daily calls, days of the week, number of weeks)
 
-//Return: the average call per month
+//Return: the average calls per month
 
 //Result for the console: The average number of calls per month are X.
 
@@ -26,12 +26,12 @@
 var total = calcAverageMonthlyCalls(60, 5, 4);
 
 function calcAverageMonthlyCalls (dailycalls , daysoftheweek, numberofweeks){
-    var averageMonthlyCalls = dailycalls * daysoftheweek / numberofweeks;
+    var averageMonthlyCalls = dailycalls * daysoftheweek * numberofweeks;
 
     return averageMonthlyCalls;
 
 }
-console.log("The average number of calls a month is " + total + " calls.");
+console.log("The average number of calls I take a month are " + total + " calls.");
 
 
 
