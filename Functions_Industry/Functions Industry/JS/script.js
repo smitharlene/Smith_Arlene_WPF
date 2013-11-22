ed6428ca0b4bcbe69fae559c67822ac779d1a874
Arlene Smith <smitharlene918@gmail.com>
2013-11-21 23:48:44 -0500

    Final Commit for Functions Industry Project

--- Functions_Industry/Functions Industry/JS/script.js
+++ Functions_Industry/Functions Industry/JS/script.js
@@ -8,19 +8,19 @@
 //Functions Industry Project
 
 
-
 //Customer Service- Inbound Calls
+
 
 //I work at a customer service call center and I need to calculate my average calls per month with the information I have.
 //Which is I take 60 calls a day, I work 5 days of the week and I work 4 weeks a month.
 
-//Calculate the amount of calls I take per month
+    //Calculate the amount of calls I take per month
 
-//Parameters (daily calls, days of the week, number of weeks)
+    //Parameters (daily calls, days of the week, number of weeks)
 
-//Return: the average calls per month
+    //Return: the average calls per month
 
-//Result for the console: "The average number of calls per month are X."
+    //Result for the console: "The average number of calls per month are X."
 
 
 var total = calcAverageMonthlyCalls(60, 5, 4);  //variable assigned to my function call that contains my arguments.
